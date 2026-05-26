6efb084f0993c7b9f46d894f87725531a4f98153
vyshnaviks05 <vyshukotha05@gmail.com>
2026-05-26 14:37:13 +0530

    Remove comment about BloodGroupCountResponseEntity
    Removed comment about renaming from BloodGroupCountResponseEntity to avoid confusion.

--- src/main/java/com/vyshnavi/dev/hospitalManagement/dto/BloodGroupCountDto.java
+++ src/main/java/com/vyshnavi/dev/hospitalManagement/dto/BloodGroupCountDto.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-// Renamed from BloodGroupCountResponseEntity — avoids confusion with Spring's ResponseEntity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
